683b18b4eea3659649ae6c16a3210b34ef87ce1d
Denis Wachira <deniswachira77@gmail.com>
2023-03-02 20:30:00 +0300

    web

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Corrects incorrect file extensions (from phpp to php) in WordPress settings file
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+This is the README.md file for the **0x17-web_stack_debugging_3** directory.
